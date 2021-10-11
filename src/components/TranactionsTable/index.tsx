09ace86a51714fd1406026ca18d6caad21d670c4
joaoMarinho94 <joaopaulim94@gmail.com>
2021-10-11 17:48:21 -0300

    feat: add list transactions

--- src/components/TranactionsTable/index.tsx
+++ src/components/TranactionsTable/index.tsx
@@ -1,10 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import api from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  category: string;
+  type: string;
+  amount: number;
+  createdAt: string;
+}
+
 export const TransactionTable: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
-    api.get("/transactions").then(({ data }) => console.log(data));
+    api
+      .get("/transactions")
+      .then(({ data }) => setTransactions(data.transactions));
   }, []);
 
   return (
@@ -20,18 +33,23 @@ export const TransactionTable: React.FC = () => {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12000</td>
-            <td>Desenvolvimento</td>
-            <td>20/32/2020</td>
-          </tr>
-          <tr>
-            <td>Alugel</td>
-            <td className="withdraw">- R$1100</td>
-            <td>Casa</td>
-            <td>20/32/2020</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
